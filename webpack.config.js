856541a92a971af6568cfbac9fddfaf86d21e41b
Aidan Slate <slate.aidan@gmail.com>
2017-08-06 21:42:50 -0400

    added gitignore

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  // context: path.resolve(__dirname, './src'),
+  entry: {
+    app: './js/entry.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.bundle.js',
+  }
+  // devtool: 'eval-source-map',
+  // module: {
+  //   loaders: [
+  //     { test: path.join(__dirname, 'js'),
+  //       loader: 'babel-loader',
+  //       query: { presets: 'es2015' } },
+  //   ]
+  // }
+};
